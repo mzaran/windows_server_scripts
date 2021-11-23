89c2fff746f1b713fceba5b9d9f86c50ce094f05
MZaran <1647324+mzaran@users.noreply.github.com>
2021-11-22 19:20:16 -0500

    Update to show formatting options and list conversion for DNS entries. Can be used for comparing against foreman list or another data source

--- scratch.py
+++ scratch.py
@@ -2,6 +2,14 @@ from winrm import Session
 
 session = Session('192.168.2.155', auth=('Administrator','SOME-long-P@$$WORD'))
 
-result = session.run_ps('Get-DnsServerResourceRecord -ZoneName "mzaran.local" -RRType "A"')
+# Show all DNS records with full details
+#result = session.run_ps('Get-DnsServerResourceRecord -ZoneName "mzaran.local" -RRType "A"')
 
-print(result.std_out.decode("utf-8"))
+# Show each DNS record for zone mzaran.local by HostName only
+result = session.run_ps('Get-DnsServerResourceRecord -ZoneName "mzaran.local" -RRType "A" | Select HostName | foreach { $_.HostName }')
+
+# Bytes output
+print(result.std_out)
+
+# Split each hostname to a string in a list
+print(result.std_out.decode("utf-8").splitlines())
